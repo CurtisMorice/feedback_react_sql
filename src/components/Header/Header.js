6b24247f68af888912156dc20ed7fa45cfe804bb
Curtis Morice <curtismorice@gmail.com>
2018-06-29 18:02:35 -0500

    installed material icon and linked Roboto

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render(){
+        return(
+            <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Prime Feedback!</h1>
+              <h4><i>Don't forget it!....if you can?</i></h4>
+            </header>
+            <br/>
+           
+          </div>
+        )
+    }
+
+}
+export default Header;
